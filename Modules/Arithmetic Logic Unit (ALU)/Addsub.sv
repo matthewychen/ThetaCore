6820d1d0fe781e8e2a619888a4d9fa66f1797066
Matthew Chen <matthewychen06@gmail.com>
2025-03-16 16:52:40 -0400

    implemented bitwise add/sub

--- Modules/Arithmetic Logic Unit (ALU)/Addsub.sv
+++ Modules/Arithmetic Logic Unit (ALU)/Addsub.sv
@@ -15,21 +15,28 @@ module AddSub(
     output AddSub_zero
 );
 
-reg [32:0] AddSub_superint;
+reg [31:0] RA_dat1;
+reg [31:0] RA_dat2;
+wire [31:0] TC_dat2;
+wire [32:0] AddSub_superint;
 
 always@(posedge soc_clk) begin
-    if (reset) begin
-        AddSub_superint <= 33'b0;
+    if (reset || ~dat_ready) begin
+        RA_dat1 <= 32'b0;
+        RA_dat2 <= 32'b0;
     end
-    else if (dat_ready) begin
+    else begin
         if (decryptedOP == 6) begin
-            AddSub_superint <= ALU_dat1 + ALU_dat2;
+            RA_dat1 <= ALU_dat1;
+            RA_dat2 <= ALU_dat2;
         end
         else if (decryptedOP == 7) begin
-            AddSub_superint <= ALU_dat1 - ALU_dat2;
+            RA_dat1 <= ALU_dat1;
+            RA_dat2 <= TC_dat2;
         end
         else begin
-            AddSub_superint <= 33'b0;
+            RA_dat1 <= 32'b0;
+            RA_dat2 <= 32'b0;
         end
     end
 end
@@ -37,5 +44,17 @@ end
 assign AddSub_out = AddSub_superint[31:0];
 assign AddSub_overflow = AddSub_superint[32];
 assign AddSub_zero = ~(|AddSub_superint[31:0]);
+
+//instantiations
+rippleadder RA(
+    .A(RA_dat1),
+    .B(RA_dat2),
+    .SUM(AddSub_superint)
+);
+
+twoscomp TC(
+    .in(ALU_dat2),
+    .out(TC_dat2)
+);
 
 endmodule
