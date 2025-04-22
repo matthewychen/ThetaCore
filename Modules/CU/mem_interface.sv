425ba9d81aeb00afa5cc19357aaa58764694ff3c
Matthew Chen <matthewychen06@gmail.com>
2025-04-22 16:00:40 -0400

    rename mem_interface

---
+++ Modules/CU/mem_interface.sv
@@ -0,0 +1,12 @@
+module mem_interface(
+    //templated
+    input soc_clk,
+    input reset,
+    input CU_ready,
+    input [3:0] bits_to_access;
+    input read_or_write;
+    //need to retrieve from and write data to SRAM.
+    output [31:0] CU_IR
+);
+
+endmodule
