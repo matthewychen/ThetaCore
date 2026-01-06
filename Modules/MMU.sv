cf5bdbfe1aeb49fb00465156c5f523dc1d45466e
Matthew Chen <matthewychen06@gmail.com>
2026-01-06 14:53:18 -0500

    update to mmu

--- Modules/MMU.sv
+++ Modules/MMU.sv
@@ -9,40 +9,47 @@ module MMU(
     input        retrieve,      // start memory op (pulse)
 
     // TO CU
-    output reg [31:0] CU_dat_out,
-    output reg        mmu_complete
+    output reg [31:0] MMU_dat_out,
 );
 
     // --------------------------------------------
     // Internal SRAM signals
     // --------------------------------------------
     reg  [6:0]  SRAM_addr_sel;
-    reg  [3:0]  SRAM_byte_sel;
     reg         read_pulse;
     reg         write_pulse;
-    reg  [31:0] SRAM_dat_in;
-
-    wire [31:0] SRAM_dat_out;
-    wire        SRAM_complete;
 
     // --------------------------------------------
     // SRAM instance
     // --------------------------------------------
-    SRAM sram_inst (
-        .addr_sel   (SRAM_addr_sel),
-        .byte_sel   (SRAM_byte_sel),
+    SRAM_sim sram_inst (
+        .addr_sel   (CU_address),
+        .byte_sel   (CU_bytesel),
         .read_pulse (read_pulse),
         .write_pulse(write_pulse),
         .datain     (SRAM_dat_in),
-        .dataout    (SRAM_dat_out),
-        .flg_complete(SRAM_complete)
+        .dataout    (MMU_dat_out)
     );
 
     // --------------------------------------------
     // MMU control FSM (stub for now)
     // --------------------------------------------
     always @(posedge soc_clk) begin
-        // to be implemented
+        SRAM_dat_in <= CU_dat_in;
+        if(retrieve) begin
+            if(read_or_write) begin //write
+                write_pulse <= 1;
+                read_pulse <= 0;
+            end
+            else begin //read
+                write_pulse <= 0;
+                read_pulse <= 1;
+            end
+        end
+        else begin
+            write_pulse <= 0;
+            read_pulse <= 0;
+        end
     end
 
 endmodule
